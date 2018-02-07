b1692454e33b64fae78f68f98451fea64a8c42c6
Casey Poon <caseycichi>
2018-02-06 18:55:06 -0800

    changes

---
+++ src/components/NavigationPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import {MdArrowBack} from 'react-icons/lib/md';
+
+const NavigationPanel = (props) => {
+
+	return (
+		<div className='NavigationPanel'>
+			<MdArrowBack onClick={props.initialState} className='back'/>
+			<div className='dots'>
+				
+				
+			</div>
+			<div style={{flex: 2}}></div>
+		</div>
+	);
+}
+
+
+
+export default NavigationPanel;
